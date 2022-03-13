67a695314032cbb5ba1da5ec211d91f444d404ae
boazfrider <boazfrider@gmail.com>
2022-03-13 21:38:43 +0200

    itemcpp

--- BarProject/BarProject/src/Item.cpp
+++ BarProject/BarProject/src/Item.cpp
@@ -1,11 +1,20 @@
 #include "../headfiles/Item.h"
 
-Item::Item(std::string name,int price) : name(name),price(price) {}
-std::string Item::getName(){return name;}
-int Item::getPrice(){return price;}
+Item::Item(std::string name , int price) : name(name) , price(price) {}
+
+std::string Item::getName() {return name;}
+
+//return the price of the item
+int Item::getPrice() {return price;}
+
+
+//change the price of the item.
 void Item::setPrice(int new_price){
     price=new_price;
 }
+
+
+//set the name of the item to new name.
 void Item::setName(std::string newname){
     name=newname;
 }
